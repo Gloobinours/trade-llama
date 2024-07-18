12234d4ee9ee4669632923ea6a5485cb097a933d
Gloobinours <kusami.louan@gmail.com>
2024-07-18 18:56:42 -0400

    websock_client js

---
+++ public/websockets_client.js
@@ -0,0 +1,24 @@
+// Connect to the WebSocket server
+const socket = new WebSocket('ws://127.0.0.1:5005');
+
+// Event listener for when the connection opens
+socket.onopen = function(event) {
+    console.log('WebSocket connected');
+    // Send a message to the server
+    socket.send('asdasd');
+};
+
+// Event listener for incoming messages
+socket.onmessage = function(event) {
+    console.log('Message from server: ', event.data);
+};
+
+// Event listener for errors
+socket.onerror = function(error) {
+    console.error('WebSocket Error: ', error);
+};
+
+// Event listener for connection closes
+socket.onclose = function(event) {
+    console.log('WebSocket closed');
+};
