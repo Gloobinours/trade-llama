fb45f81a7360677744f50043a540cb239540451e
Gloobinours <kusami.louan@gmail.com>
2024-07-18 00:19:26 -0400

    add tests

--- Tests/test_maze.py
+++ Tests/test_maze.py
@@ -1,6 +1,12 @@
 import unittest
+from src import Maze
 from src import main
 
 class TestMazeMethod(unittest.TestCase):
     def test_generate_maze(self):
-        actual = ""
+        self.assertEqual(True)
+
+    def test_generate_coins(self):
+        maze: Maze.Maze = Maze.Maze(10, 3)
+        coins = maze.generate_coins()
+        print(coins)
