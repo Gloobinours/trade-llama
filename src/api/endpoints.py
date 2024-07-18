5bb9d9c49638de8a2b2710da52c78d4899ffa744
Gloobinours <kusami.louan@gmail.com>
2024-07-17 22:22:48 -0400

    endpoints

---
+++ src/api/endpoints.py
@@ -0,0 +1,12 @@
+from flask_restful import Resource
+from flask import request, jsonify
+import json
+import sys
+sys.path.insert(0, '../')
+from Maze import Maze
+
+class MazeEndpoint(Resource):
+    def get(self, maze_size):
+        maze: Maze = Maze(maze_size)
+        mtx = maze.maze_mtx.tolist()
+        return jsonify(matrix=mtx)
