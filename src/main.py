477042c95416bd121deaa831a663a9842e4dd46d
Gloobinours <kusami.louan@gmail.com>
2024-07-18 00:20:04 -0400

    main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from Maze import Maze
 
 def main() -> None:
-    maze: Maze = Maze(16)
-    print(maze.generateMaze())
+    maze: Maze = Maze(20, 2)
+    print(maze.maze_mtx)
     
 if __name__ == "__main__":
     main()
