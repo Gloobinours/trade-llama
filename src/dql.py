2301dd9601394f8d6876e88f1d0eba9beb0de98f
AntimatterSushi <49251550+AntimatterSushi@users.noreply.github.com>
2024-08-05 22:13:37 -0400

    added ReplayMemory and DeepQNetwork classes

--- src/dql.py
+++ src/dql.py
@@ -1,10 +1,59 @@
-import torch
+from collections import deque, namedtuple
 import random
+import torch
 import torch.nn as nn
-import numpy as np
+import torch.nn.functional as F
 
+# if GPU is to be used
+device = torch.device(
+    "cuda" if torch.cuda.is_available() else
+    "mps" if torch.backends.mps.is_available() else
+    "cpu"
+)
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        """Select a random batch of transitions for training
+
+        Args:
+            batch_size (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+    
 class DeepQNetwork(nn.module):
-    def __init__(self, ):
-        super().__init__()
+    def __init__(self, n_observations, n_actions):
+        super(DeepQNetwork, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 64)
+        self.layer2 = nn.Linear(64, 64)
+        self.layer3 = nn.Linear(64, n_actions)
 
+    def forward(self, x):
+        """Called with either one element to determine next action,
+        or a batch during optimization
 
+        Args:
+            x (_type_): _description_
+
+        Returns:
+            tensor([[left0exp,right0exp]...]): Tensor
+        """
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
